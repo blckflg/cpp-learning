69643b59781ff540c69b295bea203598893520c0
blckflg <kzhmk.egor1903@gmail.com>
2025-11-24 18:33:57 +0300

    feat: show longest name

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,71 @@
-#ifdef _WIN32
-    #include <windows.h>
-#endif
-
-
+#include <windows.h>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
 
-int main(){
-    
-    #ifdef _WIN32
+size_t utf8_length(const std::string& str) {
+    size_t count = 0;
+    for (unsigned char c : str) {
+        if ((c & 0b11000000) != 0b10000000) {
+            ++count;
+        }
+    }
+    return count;
+}
+
+int main() {
+
+#ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);     //UTF-8 в консоли Windows
     system("chcp 65001 > nul");
-    #endif
+#endif
 
     vector<string> names;
+    vector<string> namesmaxlength;
 
+    //Собираем имена от пользователя
     cout << "=== Программа сбора имён ===\n";
     cout << "Вводите имена (пустая строка - завершение):\n";
-
     string name;
     while (getline(cin, name))
     {
         if (name.empty()) break;
         names.push_back(name);
     }
-    
+
     //Сортируем и выводим
     ranges::sort(names); //C++20
-    
-    cout << "\nОтсортированный список("<< names.size()<<" чел.):\n";
-    for (const auto& n: names)
+
+    cout << "\nОтсортированный список(" << names.size() << " чел.):\n";
+    for (const auto& n : names)
     {
-        cout<<"- "<<n<<'\n';
+        //Заполняем дополнительный вектор для самых длинных имен
+        if (namesmaxlength.empty() ) {
+            namesmaxlength.push_back(n);
+        }
+        else if (utf8_length(n) >= utf8_length(namesmaxlength[0])) {
+            if (utf8_length(n) > utf8_length(namesmaxlength[0])) { namesmaxlength.clear(); }
+            namesmaxlength.push_back(n);
+        }
+
+        cout << "- " << n << '\n';
     }
 
-    system ("pause"); 
+    if (namesmaxlength.size()>1) {
+        cout << "Самое длинное имя (" << utf8_length(namesmaxlength[0]) << " символов):\n";
+        cout << "- " << namesmaxlength[0]<<'\n';
+    }
+    else {
+        cout << "Самые длинные имена (" << utf8_length(namesmaxlength[0]) << " символов):\n";
+        for (const auto& n : namesmaxlength) {
+            cout << "- " << n << '\n';
+        }
+    }
+
+
+    system("pause");
     return 0;
 }
